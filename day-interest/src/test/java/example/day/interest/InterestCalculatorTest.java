5a34da50802d8b91f18980ac14f547496b0d1508
Sven Gorts <Sven.Gorts@juvo.be>
2019-07-17 22:40:45 +0200

    added offset to prevent tests to fail due to rounding errors

--- day-interest/src/test/java/example/day/interest/InterestCalculatorTest.java
+++ day-interest/src/test/java/example/day/interest/InterestCalculatorTest.java
@@ -3,13 +3,17 @@
  */
 package example.day.interest;
 
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.data.Offset.strictOffset;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InterestCalculatorTest {
+    private static final Offset<Double> EPSILON = strictOffset(0.00000000000001);
+
     private InterestCalculator interestCalculator;
 
     @BeforeEach
@@ -19,16 +23,16 @@ class InterestCalculatorTest {
 
     @Test
     void dayInterestForCasualAmount() {
-        assertThat(interestCalculator.dayInterest(10000.0)).isEqualTo(1.36986301369863);
+        assertThat(interestCalculator.dayInterest(10000.0)).isEqualTo(1.36986301369863, EPSILON);
     }
 
     @Test
     void dayInterestForBigAmount() {
-        assertThat(interestCalculator.dayInterest(250000.0)).isEqualTo(41.0958904109589);
+        assertThat(interestCalculator.dayInterest(250000.0)).isEqualTo(41.0958904109589, EPSILON);
     }
 
     @Test
     void dayInterestForNegativeAmount() {
-        assertThat(interestCalculator.dayInterest(-5000.0)).isEqualTo(-1.36986301369863);
+        assertThat(interestCalculator.dayInterest(-5000.0)).isEqualTo(-1.36986301369863, EPSILON);
     }
 }
